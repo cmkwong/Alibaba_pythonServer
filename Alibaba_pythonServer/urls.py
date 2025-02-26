0fd3e293fc84a91bb2c038f2ed4c3df483039125
aprentalchrischeung <cheung.mankwong@yahoo.com.hk>
2025-02-26 18:03:14 +0800

    added urls for app_alibaba

--- Alibaba_pythonServer/urls.py
+++ Alibaba_pythonServer/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+BASE_API = 'api/v1/query/'
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(BASE_API + 'alibaba', include('app_alibaba.urls'))
 ]
---
+++ app_alibaba/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from . import controllers
+
+urlpatterns = [
+    # path('setup', controllers.http_base_setup),
+    # path('tex', controllers.http_tex),
+    # path('write', controllers.http_write),
+    # path('remove', controllers.http_remove),
+    # path('plantcurrent', controllers.http_plant_current),
+    # path('runhour', controllers.http_plant_runHr),
+    # path('rawData', controllers.http_get_plant_raw_data)
+]
